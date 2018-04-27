00e924511d5bb6445c09636cf1b19a578b45e92a
premonish <36532816+premonish@users.noreply.github.com>
2018-04-27 19:03:15 +0800

    Add the v1 files
    Add the v1 files

---
+++ 006_lawyer/script.js
@@ -0,0 +1,8 @@
+$("#contact").click(function(){
+	alert("Call Me! +63 915 568 9151")
+	var name = prompt("Hey, what's your name, baby?");
+	// $("#name").val("name");
+	var number = prompt("Hey, " + name + ", you want to go swimming sometime? Give me your number.");
+	alert("Welcome to my world, " + name +"!")
+	// $("body").css("background", "deeppink");
+});
